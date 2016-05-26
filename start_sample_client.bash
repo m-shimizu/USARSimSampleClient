df6a3a45866640293e70d48ad4f1399c69e617b4
Masaru Shimizu <shimizu@sist.chukyo-u.ac.jp>
2016-05-27 04:01:55 +0900

    ResponseUpCameraImage

--- start_sample_client.bash
+++ start_sample_client.bash
@@ -6,21 +6,21 @@
 #  IP address of USARGazebo server
 #  robot start point
 
-roslaunch start_robot_r.launch &
+xterm -e "roslaunch start_robot_r.launch" &
 sleep 2
-roslaunch start_robot_b.launch &
+xterm -e "roslaunch start_robot_b.launch" &
 sleep 2
-roslaunch start_robot_g.launch &
+xterm -e "roslaunch start_robot_g.launch" &
 sleep 2
-roslaunch start_robot_y.launch &
-sleep 1
+xterm -e "roslaunch start_robot_y.launch" &
+sleep 2
 
 # CONVERT ROBOT CAMERA IMAGES FROM USARGazebo jpeg image TO ROS TOPIC 
 # You can change IP address of USARGazebo server in usarimage2ros.py .
 # If you change usarimage2ros.py, you must recompile it.
 #  $ python -m py_compile usarimage2ros.py
 python usarimage2ros.pyc &
-sleep 1
+sleep 2
 
 # VIEW ROBOT CAMERA IMAGES ON RVIZ
 rviz -d usarsim_client_sample.rviz &
--- usarimage2ros.py
+++ usarimage2ros.py
@@ -20,8 +20,6 @@ box_A = (0, 0, UDK_camera[1],UDK_camera[0])
 box_B = (UDK_camera[1],0,UDK_camera[1]*2,UDK_camera[0])
 box_C = (UDK_camera[1]*2,0,UDK_camera[1]*3,UDK_camera[0])
 box_D = (UDK_camera[1]*3,0,UDK_camera[1]*4,UDK_camera[0])
-#box_C = (0,UDK_camera[0],UDK_camera[1],UDK_camera[0]*2)
-#box_D = (UDK_camera[1],UDK_camera[0],UDK_camera[1]*2,UDK_camera[0]*2)
 
 clientsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientsock.connect((host,port))
@@ -30,47 +28,39 @@ clientsock.sendall(c_msg)
 
 #set image info 
 img_A=Image()
-img_A.encoding='bgr8'
+img_A.encoding='rgb8'
 img_A.is_bigendian =0
 img_A.step =1
 img_A.height = UDK_camera[0]
 img_A.width = UDK_camera[1]
 
 img_B=Image()
-img_B.encoding='bgr8'
+img_B.encoding='rgb8'
 img_B.is_bigendian =0
 img_B.step =1
 img_B.height = UDK_camera[0]
 img_B.width = UDK_camera[1]
 
 img_C=Image()
-img_C.encoding='bgr8'
+img_C.encoding='rgb8'
 img_C.is_bigendian =0
 img_C.step =1
 img_C.height = UDK_camera[0]
 img_C.width = UDK_camera[1]
 
 img_D=Image()
-img_D.encoding='bgr8'
+img_D.encoding='rgb8'
 img_D.is_bigendian =0
 img_D.step =1
 img_D.height = UDK_camera[0]
 img_D.width = UDK_camera[1]
 
-img_E=Image()
-img_E.encoding='bgr8'
-img_E.is_bigendian =0
-img_E.step =1
-img_E.height = UDK_camera[0]
-img_E.width = UDK_camera[1]
-
-
 #publish topic
-pub_A = rospy.Publisher('UDKImage_A',Image)
-pub_B = rospy.Publisher('UDKImage_B',Image)
-pub_C = rospy.Publisher('UDKImage_C',Image)
-pub_D = rospy.Publisher('UDKImage_D',Image)
-#pub_E = rospy.Publisher('UDKImage_E',Image)
+pub_A = rospy.Publisher('UDKImage_A',Image ,queue_size=1)
+pub_B = rospy.Publisher('UDKImage_B',Image ,queue_size=1)
+pub_C = rospy.Publisher('UDKImage_C',Image ,queue_size=1)
+pub_D = rospy.Publisher('UDKImage_D',Image ,queue_size=1)
+
 #start node
 rospy.init_node('UDKView')
 rcvmsg=''
@@ -88,90 +78,32 @@ while True:
                 im_b=im.crop(box_B)
                 im_c=im.crop(box_C)
                 im_d=im.crop(box_D)
-                x,y=0,0
                 smallPict_A=[]
                 smallPict_B=[]
                 smallPict_C=[]
                 smallPict_D=[]
-#                smallPict_E=[]
+                y=0
                 while y<UDK_camera[0]:
                     x=0
                     while x< UDK_camera[1]:
-                        smallPict_A.append(im_a.getpixel((x,y))[2])
-                        smallPict_A.append(im_a.getpixel((x,y))[1])
-                        smallPict_A.append(im_a.getpixel((x,y))[0])
-                        smallPict_B.append(im_b.getpixel((x,y))[2])
-                        smallPict_B.append(im_b.getpixel((x,y))[1])
-                        smallPict_B.append(im_b.getpixel((x,y))[0])
-                        smallPict_C.append(im_c.getpixel((x,y))[2])
-                        smallPict_C.append(im_c.getpixel((x,y))[1])
-                        smallPict_C.append(im_c.getpixel((x,y))[0])
-                        smallPict_D.append(im_d.getpixel((x,y))[2])
-                        smallPict_D.append(im_d.getpixel((x,y))[1])
-                        smallPict_D.append(im_d.getpixel((x,y))[0])
+                        smallPict_A.extend(im_a.getpixel((x,y)))
+                        smallPict_B.extend(im_b.getpixel((x,y)))
+                        smallPict_C.extend(im_c.getpixel((x,y)))
+                        smallPict_D.extend(im_d.getpixel((x,y)))
                         x+=1
                     y+=1
-#                x,y=0,0
-#                bb = im.getbbox()
-#                img_E.height = bb[3] 
-#                img_E.width = bb[2]
-#                while y<bb[3]:
-#                    x=0
-#                    while x<bb[2]:
-#                        smallPict_E.append(im.getpixel((x,y))[2])
-#                        smallPict_E.append(im.getpixel((x,y))[1])
-#                        smallPict_E.append(im.getpixel((x,y))[0])
-#                        x+=1
-#                    y+=1
-#                x,y=0,0
+                #smallPict_A.extend(im_a.getdata())
+                #smallPict_B.extend(im_b.getdata())
+                #smallPict_C.extend(im_c.getdata())
+                #smallPict_D.extend(im_d.getdata())
                 img_A.data = tuple(smallPict_A)
                 img_B.data = tuple(smallPict_B)
                 img_C.data = tuple(smallPict_C)
                 img_D.data = tuple(smallPict_D)
-#                img_E.data = tuple(smallPict_E)
-                #print len(img_A.data)
-#            else:   #raw data
-#                i,j=0,0
-#                smallPict=[]
-#                while i<240:
-#                    while j < 960:
-#                        smallPict.append(ord(rcvmsg[9 + j + (i * (ord(rcvmsg[5])*256 + ord(rcvmsg[6]))*3)]))
-#                        j = j+1
-#                    i=i+1
-#                    j=0 
-#                img_A.data = tuple(smallPict)
-#                i,j,smallPict=0,960,[]
-#                while i<240:
-#                    while j < 1920:
-#                        smallPict.append(ord(rcvmsg[9 + j + (i * (ord(rcvmsg[5])*256 + ord(rcvmsg[6]))*3 )]))
-#                        j = j+1
-#                    i=i+1
-#                    j=960
-#                img_B.data = tuple(smallPict)
-#                j,smallPict=0,[]
-#                while i<480:
-#                    while j < 960:
-#                        smallPict.append(ord(rcvmsg[9 + j + (i * (ord(rcvmsg[5])*256 + ord(rcvmsg[6]))*3 )]))
-#                        j = j+1
-#                    i=i+1
-#                    j=0 
-#                img_C.data = tuple(smallPict)
-#                i,j,smallPict=240,960,[]
-#                while i<480:
-#                    while j < 1920:
-#                        smallPict.append(ord(rcvmsg[9 + j + (i * (ord(rcvmsg[5])*256 + ord(rcvmsg[6]))*3 )]))
-#                        j = j+1
-#                    i=i+1
-#                    j=960 
-#                img_D.data = tuple(smallPict)
-#                print 'raw'
-#                img_E.data = tuple(rcvmsg[5:])
-
             pub_A.publish(img_A)
             pub_B.publish(img_B)
             pub_C.publish(img_C)
             pub_D.publish(img_D)
-#            pub_E.publish(img_E)
             rcvmsg=''
             clientsock.sendall(c_msg)
 #            print 'looping'
